c82f4b2cd97c571c0c0ed1ba5a08ac2d1bbd91a4
obenkucuk <obenkucuk@gmail.com>
2023-09-04 18:19:36 +0300

    fixes

--- lib/features/coin/presentation/bloc/coin_info_bloc.dart
+++ lib/features/coin/presentation/bloc/coin_info_bloc.dart
@@ -5,42 +5,24 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_clean_architecture_coin/features/coin/domain/entities/coin_info.dart';
 import 'package:flutter_clean_architecture_coin/features/coin/domain/usecases/coin_info_use_case.dart';
 
-part 'coin_info_event.dart';
 part 'coin_info_state.dart';
 
-class CoinInfoBloc extends Bloc<CoinInfoEvent, CoinInfoState> {
+class CoinInfoBloc extends Cubit<CoinInfoState> {
   final GetAllCoinInfo getAllCoinInfo;
 
   CoinInfoBloc({required this.getAllCoinInfo}) : super(CoinInfoState.initial()) {
-    on<GetCoinInfo>((event, emit) async {
-      await getCoinInfo(event, emit);
-    });
-
-    on<RefreshCoinInfo>((event, emit) async {
-      await getCoinInfo(event, emit);
-    });
+    getCoinInfo(false);
   }
 
-  Future<void> getCoinInfo(CoinInfoEvent event, Emitter<CoinInfoState> emit) async {
-    emit(state.copyWith(status: CoinInfoStatus.loading));
+  Future<void> getCoinInfo(bool isRefresh) async {
     final coinListOrFailure = await getAllCoinInfo.call();
 
-    if (event is GetCoinInfo) {
-      print('get coin info eventi geldi');
-      coinListOrFailure.match(
-        (error) => emit(state.copyWith(status: CoinInfoStatus.error)),
-        (coinList) => emit(state.copyWith(status: CoinInfoStatus.loaded, coinInfoList: coinList)),
-      );
-    }
-
-    if (event is RefreshCoinInfo) {
-      print('refresh coin info');
-      print(coinListOrFailure);
-      emit(state.copyWith(status: CoinInfoStatus.error, coinInfoList: []));
-      // coinListOrFailure.match(
-      //   (error) => emit(state.copyWith(status: CoinInfoStatus.error)),
-      //   (coinList) => emit(state.copyWith(status: CoinInfoStatus.refresh, coinInfoList: List.from(coinList))),
-      // );
-    }
+    coinListOrFailure.match(
+      (error) => emit(state.copyWith(status: CoinInfoStatus.error)),
+      (coinList) => emit(state.copyWith(
+        status: isRefresh ? CoinInfoStatus.refresh : CoinInfoStatus.loaded,
+        coinInfoList: List.of(coinList),
+      )),
+    );
   }
 }
--- lib/features/coin/presentation/pages/coin_info_screen.dart
+++ lib/features/coin/presentation/pages/coin_info_screen.dart
@@ -10,96 +10,69 @@ class CoinInfoScreen extends StatelessWidget {
   const CoinInfoScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            title: const Text('Coin Statistics'),
-            bottom: PreferredSize(
-              preferredSize: Size.fromRadius(MediaQuery.sizeOf(context).height * 0.01),
-              child: const ListTitles(),
+  BlocProvider<CoinInfoBloc> build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => sl.get<CoinInfoBloc>(),
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              title: const Text('Coin Statistics'),
+              bottom: PreferredSize(
+                preferredSize: Size.fromRadius(MediaQuery.sizeOf(context).height * 0.01),
+                child: const ListTitles(),
+              ),
             ),
-          ),
-          SliverRefreshWidget(
-            onRefresh: () {
-              sl.get<CoinInfoBloc>().add(const RefreshCoinInfo());
-              return Future.delayed(const Duration(seconds: 3));
-            },
-            isRefreshing: true,
-          ),
-          BlocBuilder<CoinInfoBloc, CoinInfoState>(
-            bloc: context.read<CoinInfoBloc>(),
-            builder: (context, state) {
-              if (state.status == CoinInfoStatus.loading) {
-                return const SliverFillRemaining(
-                  child: CircularProgressIndicator.adaptive(),
-                );
-              } else if (state.status == CoinInfoStatus.loaded) {
-                print('Widget loaded yeri');
+            Builder(builder: (context) {
+              return SliverRefreshWidget(
+                onRefresh: () => Future.wait([context.read<CoinInfoBloc>().getCoinInfo(true)]),
+                isRefreshing: true,
+              );
+            }),
+            BlocBuilder<CoinInfoBloc, CoinInfoState>(
+              builder: (context, state) {
+                if (state.status == CoinInfoStatus.loading) {
+                  return const SliverFillRemaining(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+                } else if (state.status == CoinInfoStatus.loaded || state.status == CoinInfoStatus.refresh) {
+                  print('Widget loaded yeri');
 
-                return SliverPadding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  sliver: SliverList.builder(
-                    itemBuilder: (context, index) {
-                      final item = state.coinInfoList![index];
-                      final changePercent24Hr = double.parse(item.changePercent24Hr ?? '0.0');
-                      final color = changePercent24Hr > 0
-                          ? Colors.green
-                          : changePercent24Hr == 0
-                              ? Colors.grey
-                              : Colors.red;
+                  return SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    sliver: SliverList.builder(
+                      itemBuilder: (context, index) {
+                        final item = state.coinInfoList![index];
+                        final changePercent24Hr = double.parse(item.changePercent24Hr ?? '0.0');
+                        final color = changePercent24Hr > 0
+                            ? Colors.green
+                            : changePercent24Hr == 0
+                                ? Colors.grey
+                                : Colors.red;
 
-                      return SingleListItem(
-                        color: color,
-                        changePercent24Hr: changePercent24Hr,
-                        priceUsd: item.priceUsd,
-                        name: item.name,
-                        symbol: item.symbol,
-                        volumeUsd24Hr: item.volumeUsd24Hr,
-                      );
-                    },
-                    // itemExtent: 150,
-                    itemCount: state.coinInfoList!.length,
-                  ),
-                );
-              } else if (state.status == CoinInfoStatus.refresh) {
-                print('Widget refresh yeri');
-
-                return SliverPadding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  sliver: SliverList.builder(
-                    itemBuilder: (context, index) {
-                      final item = state.coinInfoList![index];
-                      final changePercent24Hr = double.parse(item.changePercent24Hr ?? '0.0');
-                      final color = changePercent24Hr > 0
-                          ? Colors.green
-                          : changePercent24Hr == 0
-                              ? Colors.grey
-                              : Colors.red;
-
-                      return SingleListItem(
-                        color: color,
-                        changePercent24Hr: changePercent24Hr,
-                        priceUsd: item.priceUsd,
-                        name: item.name,
-                        symbol: item.symbol,
-                        volumeUsd24Hr: item.volumeUsd24Hr,
-                      );
-                    },
-                    // itemExtent: 150,
-                    itemCount: state.coinInfoList!.length,
-                  ),
-                );
-              } else if (state.status == CoinInfoStatus.error) {
-                return const SliverFillRemaining(child: Text('Bilinmeyen bir hata oluştu!'));
-              } else {
-                return const SliverFillRemaining(child: Text('Bilinmeyen bir hata oluştu!'));
-              }
-            },
-          )
-        ],
+                        return SingleListItem(
+                          color: color,
+                          changePercent24Hr: changePercent24Hr,
+                          priceUsd: item.priceUsd,
+                          name: state.status.name,
+                          symbol: item.symbol,
+                          volumeUsd24Hr: item.volumeUsd24Hr,
+                        );
+                      },
+                      // itemExtent: 150,
+                      itemCount: state.coinInfoList!.length,
+                    ),
+                  );
+                } else if (state.status == CoinInfoStatus.error) {
+                  return const SliverFillRemaining(child: Text('Bilinmeyen bir hata oluştu!'));
+                } else {
+                  return const SliverFillRemaining(child: Text('Bilinmeyen bir hata oluştu!'));
+                }
+              },
+            )
+          ],
+        ),
       ),
     );
   }
